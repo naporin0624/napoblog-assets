a9f9ce42b5851a338902663a1a0a58f039929983
napori0624 <naporin0624@gmail.com>
2020-01-17 21:22:18 +0900

    :sparkles: routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ListTransition } from "./components/ListTransition";
+import { LottieCheckBox } from "./components/LottieCheckBox";
 
 export const Router = () => {
   return (
@@ -8,6 +9,9 @@ export const Router = () => {
       <Switch>
         <Route path="/list-transition">
           <ListTransition />
+        </Route>
+        <Route path="/">
+          <LottieCheckBox />
         </Route>
       </Switch>
     </BrowserRouter>

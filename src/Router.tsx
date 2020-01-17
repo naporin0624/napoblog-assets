72054f2667b5734172173dee483c22bd1d9d852b
napori0624 <naporin0624@gmail.com>
2020-01-17 23:13:47 +0900

    :+1: routing

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { ListTransition } from "./components/ListTransition";
 import { LottieCheckBox } from "./components/LottieCheckBox";
+import { DivBlur } from "./components/DivBlur";
 
 export const Router = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/div-blur">
+          <DivBlur />
+        </Route>
         <Route path="/list-transition">
           <ListTransition />
         </Route>
